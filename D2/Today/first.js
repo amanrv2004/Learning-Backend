6af128e87ea5a9a84c71c943ca12a5fa04c212e1
Aman Raj Verma <amanrv2004@gmail.com>
2026-06-30 10:25:35 +0530

    3

--- D2/Today/first.js
+++ D2/Today/first.js
@@ -1,6 +1,13 @@
 
 const sum = require("./current/sum")
+const sub = require("./current/sub")
+const mul = require("./current/mul")
 
-//sum
+sum(2,4)
 //sub
+sub(2,4)
 //mul
+mul(2,4)
+
+
+console.log("Hello Ji")
