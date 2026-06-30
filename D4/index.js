bba41710d484f3cae3ccffc0f6ea30cd07aeda56
Aman Raj Verma <amanrv2004@gmail.com>
2026-06-30 19:26:51 +0530

    8

--- D4/index.js
+++ D4/index.js
@@ -2,7 +2,21 @@
 const http = require('http')
 
 const server = http.createServer((req,res)=>{
-    res.end("Hello Man")
+    
+
+    if(req.url ==="/"){
+        res.end("Hello Aman");
+    }
+    else if (req.url ==="/contact"){
+        res.end("This is our Contact Page.");
+    }
+    else if (req.url ==="/about"){
+        res.end("This is our About Page.");
+    }
+    else{
+        res.end("Error: Page not found");
+    }
+
 });
 
 const port = 4000;
