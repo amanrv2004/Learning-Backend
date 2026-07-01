356025739b8591d87897162d8705b2d51e1a0b70
Aman Raj Verma <amanrv2004@gmail.com>
2026-07-01 11:13:22 +0530

    3

---
+++ D7 Postman/index.js
@@ -0,0 +1,34 @@
+
+const express = require("express");
+
+const app = express();
+
+// app.use("/user",(req,res)=>{
+//     res.send("Hello Coder Army")
+// })
+
+//get  
+
+
+// parsing 
+
+app.use(express.json());
+
+app.get("/user",(req,res)=>{
+    // console.log(req);
+    res.send({name:"Aman"})
+
+})
+
+app.post("/user",(req,res)=>{
+    console.log("Data send Successfully");
+    console.log(req.body);
+    res.send("Data Saved Successfully");
+})
+
+
+
+
+app.listen(4000,()=>{
+    console.log(`http://localhost:4000`);
+})
