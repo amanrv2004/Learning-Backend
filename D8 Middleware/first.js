6ce1386de548162f54c422a3039fc7b347348008
Aman Raj Verma <amanrv2004@gmail.com>
2026-07-04 11:03:47 +0530

    2

--- D8 Middleware/first.js
+++ D8 Middleware/first.js
@@ -3,15 +3,41 @@ const express = require("express");
 const app = express();
 
 
-app.use("/user",(req,res)=>{
-    console.log("first");
-    res.send("Hello Gi");
-    console.log("First first");
-    // res.send("Hello Gi mai must ho");
-},(req,res)=>{
-    console.log("second");
-    res.send("Hello I am Second");
+// app.use("/user",[(req,res,next)=>{
+//     console.log("first");
+//     //res.send("Hello Gi");
+//     next();
+// },(req,res,next)=>{
+//     console.log("second");
+//     // res.send("Hello I am Second");
+//     next();
+// },(req,res,next)=>{
+//     console.log("third");
+//     res.send("Hello I am Third");
+// }])
+
+
+app.use("/user",(req,res,next)=>{
+    console.log(`${Date.now()} ${req.method} ${req.url}`)
+    next();
 })
+
+app.get("/user",(req,res)=>{
+    
+    res.send("Info about User");
+});
+
+app.post("/user",(req,res)=>{
+   
+    res.send("Information Saved");
+});
+
+app.delete("/user",(req,res)=>{
+   
+    res.send("Info Deleted Successfully");
+});
+
+
 
 
 
